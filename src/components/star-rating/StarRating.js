55d178667702d57cb95eb5894ad44ef51bc4c25a
Jennifer Fu <jenniferfu@Jennifers-MacBook-Pro.local>
2024-10-05 09:42:41 -0700

    Add implementation

---
+++ src/components/star-rating/StarRating.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './styles.css';
+
+export const StarRating = ({ numOfStars = 5 }) => {
+  const [rating, setRating] = useState(-1);
+  const [hover, setHover] = useState(-1);;
+
+  return (
+    <div className="container">
+      <div className="star-container">
+        {[...Array(numOfStars)].map((_, index) => (
+          <div
+            key={index}
+            className={index <= Math.max(rating, hover) ? 'active-star' : 'inactive-star'}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(-1)}
+          >
+            ★
+          </div>
+        ))}
+      </div>
+      {rating === -1 ? 'Not Rated' : `Rated ${rating + 1}`}
+    </div>
+  );
+};
