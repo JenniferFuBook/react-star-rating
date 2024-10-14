56f1681f3bdfbd360daf8410b2eca830b4f791b9
Jennifer Fu <jenniferfu@Jennifers-MacBook-Pro.local>
2024-10-14 09:42:24 -0700

    Add more customization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { StarRating } from './components/star-rating/StarRating';
 
 function App() {
-  return <StarRating numOfStars={5} />;
+  return (
+    <StarRating
+      numOfStars={5}
+      // activeColor='red'
+      // inactiveColor='black'
+      // starSize='4em'
+    />);
 }
 
 export default App;
--- src/components/star-rating/StarRating.js
+++ src/components/star-rating/StarRating.js
@@ -1,9 +1,40 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './styles.css';
 
-export const StarRating = ({ numOfStars = 5 }) => {
-  const [rating, setRating] = useState(-1);
+export const StarRating = ({
+  // Customization option for the current rating
+  currentRating = -1,
+
+  // Customization option for the number of stars
+  numOfStars = 5,
+
+  // Customization option for star active color
+  activeColor = 'orange',
+
+  // Customization option for star inactive color
+  inactiveColor = 'gray',
+
+  // Customization option for star size
+  starSize = '40px',
+}) => {
+  // State management to track the current rating
+  const [rating, setRating] = useState(currentRating);
+
+  // State management to track the hover state
   const [hover, setHover] = useState(-1);
+
+  // Get star customization for color, size, and cursor
+  const getStarStyle = useCallback((isActive) => (
+    isActive ? {
+      color: activeColor,
+      fontSize: starSize,
+      cursor: 'pointer'
+    } : {
+      color: inactiveColor,
+      fontSize: starSize,
+      cursor: 'default'
+    }
+  ), [activeColor, inactiveColor, starSize]);
 
   return (
     <div className="container">
@@ -11,8 +42,14 @@ export const StarRating = ({ numOfStars = 5 }) => {
         {[...Array(numOfStars)].map((_, index) => (
           <div
             key={index}
-            className={index <= Math.max(rating, hover) ? 'active-star' : 'inactive-star'}
+
+            // Star customization for color, size, and cursor
+            style={getStarStyle(index <= Math.max(rating, hover))}
+
+            // Clicking interaction
             onClick={() => setRating(index)}
+
+            // Hovering interaction
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(-1)}
           >
